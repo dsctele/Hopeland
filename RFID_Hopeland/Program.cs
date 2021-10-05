069f70ec95ed5a5a3ab7bd0129d9d9711c3cdfaf
Diego Schernoski <dschernoski@bionexo.com>
2021-10-05 17:03:25 -0300

    hopeland

---
+++ RFID_Hopeland/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RFID_Hopeland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        static RFIDReaderAPI.Param_Option PARAM_SET = new RFIDReaderAPI.Param_Option();
+        static RFIDReaderAPI.RFID_Option RFID_OPTION = new RFIDReaderAPI.RFID_Option();
+ 
+        {
+            RFIDReaderAPI.Interface.IAsynchronousMessage log = new Program();
+            if (RFIDReaderAPI.RFIDReader.CreateTcpConn("192.168.1.116:9090", log))
+            // TCP Connect
+            {
+                Console.WriteLine("Create connection successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Create connection failed!");
+            }
+        }
+    }
